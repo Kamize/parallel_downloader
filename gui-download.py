1c67eebb65459c7d3ed6b90407d20a3fcb7a8f6b
kamize <talithanabila4c@gmail.com>
2023-01-03 09:57:01 +0700

    add missing module

--- gui-download.py
+++ gui-download.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+import tkinter.ttk as ttk
+from tkinter.ttk import Progressbar
 
 class DownloadManagerGUI:
     def __init__(self, master):
@@ -10,20 +12,20 @@ class DownloadManagerGUI:
         self.url_entry = tk.Entry(self.master)
         self.download_button = tk.Button(self.master, text="Download", command=self.download)
         self.progress_label = tk.Label(self.master, text="Progress:")
-        #self.progress_bar = tk.Progressbar(self.master, orient="horizontal", length=200, mode="determinate")
+        self.progress_bar = ttk.Progressbar(self.master, orient="horizontal", length=200, mode="determinate")
         
         self.url_label.pack()
         self.url_entry.pack()
         self.download_button.pack()
         self.progress_label.pack()
-        #self.progress_bar.pack()
+        self.progress_bar.pack()
     
     def download(self):
         # get the URL from the entry widget
         url = self.url_entry.get()
         
         # download the file here and update the progress bar as the download progresses
-        # self.progress_bar['value'] = current_progress
+        #self.progress_bar['value'] = current_progress
 
 root = tk.Tk()
 app = DownloadManagerGUI(root)
